35a6df2d7f3b12ad5d0740bc94de22aa233da6d5
insert-usernamehere <montyd18@gmail.com>
2020-07-20 11:47:36 -0400

    finally its commiting

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ const bot = new Discord.Client();
 
 const token = "bot id";
 
-
 bot.on("ready", () => {
   console.log("bot is working");
 });
 
 bot.on("message", async msg => {
-  if (msg.content === "anime") {
-    msg.reply("anime sucks");
+  if (msg.content === "days since direct") {
+    msg.reply("its been 365 days since last direct");
   }
 
   if (msg.content === "useless info") {
